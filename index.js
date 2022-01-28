772ee64e8d9af8b4f5203a4c18e8c7f290abbe84
Your Name <tahrimurmoon@gmail.com>
2022-01-28 07:43:08 +0600

    database connected successfully

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+const {MongoClient} = require('mongodb');
+const cors = require('cors');
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// mongodb atlas database server connection
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.budis.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+    try {
+        await client.connect();
+        console.log('database connected successfully');
+    } finally {
+        await client.close();
+    }
+}
+
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('server side app running');
+});
+
+app.listen(port, () => {
+    console.log(`server side app listening at port:${port}`);
+});
